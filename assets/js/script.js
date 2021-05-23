d4555fae1fafe21dd39bc5283f7ec97c2e700347
canitram <ma.covarrubias.a@gmail.com>
2021-05-23 01:14:02 -0400

    smoothScroll y bg-nav al navegar

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,11 @@
 $(document).ready(function () {
 
-});
+    $(window).scroll(function(){
+        if($('#navbarra').offset().top > 580 ){
 
+            $('#navbarra').addClass('bg-scroll');
+        }else{
+            $('#navbarra').removeClass('bg-scroll');
+        }
+    });
+});
